145018408b4c9d01b274b021a84b850498983efd
andziooooi <andrzyk@gmail.com>
2024-11-27 01:39:33 +0100

    fixed bug with deadline editing

--- ToDoList/ViewModels/MainViewModel.cs
+++ ToDoList/ViewModels/MainViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Globalization;
 using System.Runtime.CompilerServices;
 using System.Windows.Input;
 using ToDoList.Commands;
